831b9910837934fb36637136629edb9420fb810b
abidkk <dev.abidkk@gmail.com>
2024-02-29 10:43:36 +0500

    inpout output

---
+++ 03_input_output/inp-out.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int ammount ;
+
+    // output
+    cout << "Please enter ammount:  ";
+
+    // getting input 
+    cin >> ammount;
+
+    // output
+    cout << "Output is " << ammount << endl;
+
+    return 0;
+
+}

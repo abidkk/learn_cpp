39d688c93cf7f21ad9a5a524dbaaf4089bf1ccae
abidkk <dev.abidkk@gmail.com>
2024-03-05 13:02:02 +0500

    functions

---
+++ 08_functions/01_func.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+char hello(){
+    // char message = *"Hello, world!";
+    char message = *"Hello world!";
+    return message;
+}
+
+int main() {
+
+cout << hello();
+
+return 0;
+}
